22832b0b00ccf00a8b4f9bd1f4bf5f9de4cb657c
sabatms <110444941+sabatms@users.noreply.github.com>
2024-05-20 13:06:52 +0330

    react.js

--- episode-3/src/App.js
+++ episode-3/src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
 
 function App() {
+
+  const [age , setAge] = useState(0)
+  //
+
+  const [inputValue ,setInputValue] =useState("")
+  const handelInputChang =(event) =>{
+    setInputValue(event.target.value)
+  }
+  //
+
+  const [showText ,setShowText] = useState(true)
+  const toggleText =() =>{
+   setShowText(!showText)
+  }
+  //
+
+  const [textcolor , setTextColor] = useState("red")
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+    <h1>{age}</h1>
+    <button onClick={() => setAge(age+1)}> Increase age</button>
+    <button onClick={() => setAge(age-1)}> Decrease age</button>
+
+    <hr></hr>
+    <br></br>
+
+    <input type='text' onChange={handelInputChang}></input>
+    <h1>{inputValue}</h1>
+
+    <hr></hr>
+    <br></br>
+    
+    <button onClick={toggleText} >Hide/Show</button>
+    {showText && <h1>SabaTms Codding</h1>}
+
+    <hr></hr>
+    <br></br>
+    
+    <button onClick={() => setTextColor (textcolor === "red" ? "green" : "red")}>Change Color</button>
+    <h1 style={{color:textcolor}}>SabaTms Codding</h1>
     </div>
   );
 }
