b4c4cf0c25db76bb172a39e751e62950b3f28b2d
sabatms <110444941+sabatms@users.noreply.github.com>
2024-05-20 12:19:15 +0330

    react.js

--- episode-2/src/App.js
+++ episode-2/src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const age = 23
+  const names =["Saba" ,"Fatemeh" , "Soha" , "Sanam"]
+  const users = [
+    {name : "Ali" , age : 55 },
+    {name : "Reza" , age : 99 },
+    {name : "Khosii" , age : 12 }
+  ]
+
+  const classes = [
+   {name : "Html" , finished : true},
+   {name : "JavaScript" , finished : false},
+   {name : "Css" , finished : true},
+   {name : "Bootstrap" , finished : false},
+   {name : "React" , finished : true},
+   {name : "Python" , finished : false},
+  ]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     {age > 20 ? <h1>Over</h1> : <h1> Under</h1>}
+     {age === 23 && <h2>age is 23</h2>}  
+
+     <button type= "submit" className={age > 20 ? "success" : "warnning"} >Click me</button>
+
+     <br></br>
+     <br></br>
+
+     <button type="submit" style={{fontSize : age > 20 ? "20px" :"30px"}} >Click me</button>
+
+     <hr></hr>
+    
+    {names.map((name, index) =>{
+      return <h1 key={index}> {name} </h1>
+    })}
+
+    <hr></hr>
+
+    {users.map((user, index) =>{
+      return <h2 key={index}>{user.name} : {user.age} </h2>
+    })}
+   
+   <hr></hr>
+
+   {classes.map((c, index) =>{
+    return c.finished && <h2 key ={index}>{c.name}</h2>
+   })}
+
     </div>
   );
 }
